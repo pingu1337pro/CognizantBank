e3c5e52668f7b382f9b10a02cda64a37a95cd4dd
KSSA <adrian.kessler@cognizant-mobility.com>
2022-11-15 07:52:33 +0100

    KROP in tha houze

--- src/main/java/bank/Main.java
+++ src/main/java/bank/Main.java
@@ -1,13 +1,15 @@
 package bank;
 
-import bank.business.Bank;
-import bank.client.CommandLineClient;
-import resources.PasswordHasher;
+import bank.business.BankSystem;
+import bank.client.CLI.CommandLineClient;
+import bank.client.SwingGUI.SwingGuiClient;
+import bank.core.Bank;
 
 public class Main {
     public static void main(String[] args) {
         Bank bank = new Bank();
-        CommandLineClient client = new CommandLineClient(bank);
-        client.runBank();
+        BankSystem bankSystem = new BankSystem(bank);
+        new SwingGuiClient(bankSystem);
+        new CommandLineClient(bankSystem);
     }
 }
