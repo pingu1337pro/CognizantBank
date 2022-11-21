1058bd3f324c7b11dcbaf16164a51e2c3fa0f53b
pingu1337pro <adriankessler@protonmail.com>
2022-11-21 15:47:24 +0100

    macOS shit; berufsschule

---
+++ src/main/java/bank/business/BankLoggerMac.java
@@ -0,0 +1,38 @@
+package bank.business;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ *Generates a text file with anything the LogHandler sends him.
+ */
+public class BankLoggerMac {
+    private Logger logger = Logger.getLogger(BankLoggerMac.class.getName());
+    private FileHandler fh = null;
+
+    public BankLoggerMac(String name) {
+        SimpleDateFormat format = new SimpleDateFormat("M-d_HH-mm-ss");
+        try {
+            fh = new FileHandler("C:/Users/KSSA/Desktop/MyLogFile_" + name + format.format(Calendar.getInstance().getTime()) + ".log");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        fh.setFormatter(new SimpleFormatter());
+        logger.setLevel(java.util.logging.Level.ALL);
+        //TODO: Uncomment next line to disable console logging; useful for Bank testing purposes
+        //logger.setUseParentHandlers(false);
+        logger.addHandler(fh);
+    }
+
+
+    public void doInfoLogging(String info) {
+        logger.info(info);
+    }
+
+    public void doErrorLogging(String error) {
+        logger.severe(error);
+    }
+}
